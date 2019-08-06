10ff08996fa0ba4c48a4f8409111136f8958e9d9
pi2017 <savskiev@gmail.com>
2019-08-06 23:16:34 +0300

    Client send msg

---
+++ client.py
@@ -0,0 +1,27 @@
+
+import socket
+
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1241))
+
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print('new msg len: ', msg[:HEADERSIZE])
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        print(f'full message length: {msglen}')
+        full_msg += msg.decode('utf-8')
+        print(len(full_msg))
+
+        if len(full_msg) - HEADERSIZE == msglen:
+            print('All message recvd')
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
